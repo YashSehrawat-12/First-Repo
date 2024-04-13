93a4484893752bb3743df5cc9f8fe9257405dd62
YashSehrawat-12 <yash33273@gmail.com>
2024-04-13 16:44:33 +0530

    this was updated for mobile phone details

---
+++ 2nd_prog.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"hello this is iPhone!!"<<endl;
+    cout<<"nice to meet you all!!"<<endl;
+    return 0;
+}
--- hello.cpp
+++ hello.cpp
@@ -2,5 +2,6 @@
 using namespace std;
 int main(){
     printf("hello there!!");
+    printf("this is a program of c language!!\n");
     return 0;
 }
